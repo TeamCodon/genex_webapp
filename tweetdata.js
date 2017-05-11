6d850d1af069a8751838935b34e4a2f9c1b5345b
Vindula Jayawardana <vindulamj@gmail.com>
2017-05-11 15:33:46 +0530

    Add Tweet Analysis

---
+++ tweetdata.js
@@ -0,0 +1,2 @@
+var tweetPoints = [['8/23/15', 6.927079, 79.861243,"Extra virgin olive oil is just like regular olive oil but with more Star Wars action figures."],
+    ['9/6/15 8:19', 40.712784, -74.005941,"I don't understand how God can have Ten Commandments for the whole world, and my wife can have 152 just for our house."]]
