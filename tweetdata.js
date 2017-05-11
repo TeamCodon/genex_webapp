c9195b666b66953eb933b5cbc65823c7c27a183d
Vindula Jayawardana <vindulamj@gmail.com>
2017-05-11 19:08:45 +0530

    Final Tweet Analysis

--- tweetdata.js
+++ tweetdata.js
@@ -1,2 +1,4 @@
-var tweetPoints = [['8/23/15', 6.927079, 79.861243,"Extra virgin olive oil is just like regular olive oil but with more Star Wars action figures."],
-    ['9/6/15 8:19', 40.712784, -74.005941,"I don't understand how God can have Ten Commandments for the whole world, and my wife can have 152 just for our house."]]
+var tweetPoints = [['2015', 6.927079, 79.861243,"Extra virgin olive oil is just like regular olive oil but with more Star Wars action figures."],
+    ['2014', 6.129, 79.861243,"Extra virgin olive oil is just like regular olive oil but with more Star Wars action figures."],
+    ['1985', 40.0, -74.905941,"Extra virgin olive oil is just like regular olive oil but with more Star Wars action figures."],
+    ['1985', 40.712784, -74.005941,"I don't understand how God can have Ten Commandments for the whole world, and my wife can have 152 just for our house."]]
