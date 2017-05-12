10f4c5b5490cf4dedcf2c5fcb8302e7fe15c771f
Janaka Chathuranga <bjchathuranga@gmail.com>
2017-05-12 17:45:07 +0530

    adding populations

--- scripts/convert_population_data_to_json.py
+++ scripts/convert_population_data_to_json.py
@@ -3,6 +3,7 @@
 
 
 import pandas as pd
+import math
 
 df_pop = pd.read_csv('../../../data/population-density-3.csv-Sheet3-1.csv', low_memory=False)
 
@@ -14,11 +15,17 @@ file.write("var populations_points = [");
 df = df_pop[:]
 
 #print(df.loc[:100,['timestamp','tag-local-identifier','individual-local-identifier']])
-
+labels = 'Year','Latitude','Longitude','Population Density'
     
 for index, point in df.iterrows():
     #print(point['location-lat'])
-    file.write('[\''+str(point['Year'])+'\', '+ str(point['Latitude'])+ ', '+str(point['Longitude'])+', '+str(point['Population Density'])+ '],\n')
+    skip = False
+    for l in labels:
+        if math.isnan(point[l]):
+            skip =True
+            continue
+    if not skip:
+        file.write('[\''+str(point['Year'])+'\', '+ str(point['Latitude'])+ ', '+str(point['Longitude'])+', '+str(point['Population Density'])+ '],\n')
     
 
 
